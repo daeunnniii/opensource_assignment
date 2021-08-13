a5330d4dc731e2ac8ca92bc9abfa0cb817ef751d
daeunnniii <storyda1@naver.com>
2021-08-13 18:12:59 +0900

    feat: templates, static 추가 및 로그인 연결

--- miraclenote/settings.py
+++ miraclenote/settings.py
@@ -129,3 +129,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 로그인 성공후 이동하는 URL
+LOGIN_REDIRECT_URL = '/mynote/'
