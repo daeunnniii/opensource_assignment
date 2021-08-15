59553d8b2297b6705934ace34ad8b372f629c0db
daeunnniii <storyda1@naver.com>
2021-08-16 03:26:54 +0900

    feat: 회원가입 추가, 홈화면 url 매핑

--- common/views.py
+++ common/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from common.forms import UserForm
 
-# Create your views here.
+def index(request):
+    return render(request, 'common/home.html')
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('common:login')
+    else:
+        form = UserForm()
+    return render(request, 'common/signup.html', {'form': form})
--- miraclenote/settings.py
+++ miraclenote/settings.py
@@ -132,3 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # 로그인 성공후 이동하는 URL
 LOGIN_REDIRECT_URL = '/mynote/'
+
+# 로그아웃시 이동하는 URL
+LOGOUT_REDIRECT_URL = '/'
